30f450aa7896f29785e0664498f527fd81d34ba1
jonathanpriyaraj <jonathanpriyaraj37@gmail.com>
2026-02-26 14:08:06 +0000

    added files

---
+++ 13.null-local-exec.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "webservers" {
+  provisioner "local-exec" {
+    command = <<EOH
+      sleep 10
+      ANSIBLE_HOST_KEY_CHECKING=False ansible -i invfile pvt -m ping
+    EOH
+  }
+
+  depends_on = [local_file.ansible-inventory-file]
+}
+
